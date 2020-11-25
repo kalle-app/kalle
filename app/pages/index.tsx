830655fde76aa874a0e0a9af8c580c3566af4b04
Philip Weidenfeller <philip.weidenfeller@student.hpi.de>
2020-11-25 23:44:14 +0100

    silence linter warnings

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,49 +1,47 @@
-import { Link, BlitzPage, useMutation } from "blitz"
+import { BlitzPage } from "blitz"
 import Layout from "app/layouts/Layout"
-import logoutMutation from "app/auth/mutations/logout"
-import { useCurrentUser } from "app/hooks/useCurrentUser"
 import NavBar from "app/components/navbar"
 import { PrimaryButton, SecondaryButton, TertiaryButton } from "app/components/buttons"
 
-const UserInfo = () => {
-  const currentUser = useCurrentUser()
-  const [logout] = useMutation(logoutMutation)
+// const UserInfo = () => {
+//   const currentUser = useCurrentUser()
+//   const [logout] = useMutation(logoutMutation)
 
-  if (currentUser) {
-    return (
-      <>
-        <button
-          className="button small"
-          onClick={async () => {
-            await logout()
-          }}
-        >
-          Logout
-        </button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-        </div>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Link href="/signup">
-          <a className="button small">
-            <strong>Sign Up</strong>
-          </a>
-        </Link>
-        <Link href="/login">
-          <a className="button small">
-            <strong>Login</strong>
-          </a>
-        </Link>
-      </>
-    )
-  }
-}
+//   if (currentUser) {
+//     return (
+//       <>
+//         <button
+//           className="button small"
+//           onClick={async () => {
+//             await logout()
+//           }}
+//         >
+//           Logout
+//         </button>
+//         <div>
+//           User id: <code>{currentUser.id}</code>
+//           <br />
+//           User role: <code>{currentUser.role}</code>
+//         </div>
+//       </>
+//     )
+//   } else {
+//     return (
+//       <>
+//         <Link href="/signup">
+//           <a className="button small">
+//             <strong>Sign Up</strong>
+//           </a>
+//         </Link>
+//         <Link href="/login">
+//           <a className="button small">
+//             <strong>Login</strong>
+//           </a>
+//         </Link>
+//       </>
+//     )
+//   }
+// }
 
 const Home: BlitzPage = () => {
   return (
