5637ec6319817bf5479459b992217ccfcb1882be
Simon Knott <info@simonknott.de>
2020-11-25 07:47:26 +0100

    Remind dev to start baikal for tests

--- app/caldav/index.spec.ts
+++ app/caldav/index.spec.ts
@@ -1,4 +1,5 @@
 import { freeBusy } from "./"
+import fetch from "node-fetch"
 
 const baikalJohnDoe = {
   url: "http://localhost:5232/dav.php/calendars/john.doe/default/",
@@ -8,6 +9,19 @@ const baikalJohnDoe = {
     digest: true,
   },
 }
+
+describe("baikal test instance", () => {
+  it("is available", async () => {
+    try {
+      const response = await fetch("http://localhost:5232")
+      expect(response.status).toEqual(200)
+    } catch (error) {
+      throw new Error(
+        "Baikal not available. Run `npm run test:baikal:start` to start the container."
+      )
+    }
+  })
+})
 
 describe("freeBusy", () => {
   it("returns a freeBusy schedule", async () => {
