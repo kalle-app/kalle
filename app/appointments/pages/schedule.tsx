f196f4864ec4ad0fa9bac0055e24e3e308eaa9e5
Rohan Sawahn <rohan.sawahn@student.hpi.uni-potsdam.de>
2020-12-01 17:21:42 +0100

    started invitee view

--- app/appointments/pages/schedule.tsx
+++ app/appointments/pages/schedule.tsx
@@ -1,6 +1,5 @@
 import { BlitzPage, useQuery } from "blitz"
 import React, { Suspense, useState } from "react"
-import { Card, Container } from "react-bootstrap"
 import getAvailableSlots from "../queries/getAvailableSlots"
 
 const Scheduler = () => {
@@ -12,20 +11,29 @@ const Scheduler = () => {
   }
 
   return (
-    <Container>
-      <Card>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-    </Container>
+    <div className="container mx-auto p-4 mt-5">
+      <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Schedule an Appointment</h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            Select a timeslot of your preference
+          </p>
+        </div>
+        <div className="border-t border-gray-200">
+          <div className="grid grid-cols-3 gap-6">
+            <div className="flex p-4 col-span-1  border-right border-r-2 border-gray-200">
+              <div>Title, Description and other general stuff here</div>
+            </div>
+            <div className="flex col-span-2">
+              <div>
+                calendar here with data with free slots highlighted. On click on date show available
+                slots
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
