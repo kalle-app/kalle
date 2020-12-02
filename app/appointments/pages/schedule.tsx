b2247a115a0e6928fdbe16c3fbe06de9a9b654e4
Stefan Spangenberg <stefan.spangenberg@student.hpi.de>
2020-12-02 13:01:18 +0100

    Integrate basic calender view
    Co-authored-by: Philip Weidenfeller <weidenfeller@posteo.de>

--- app/appointments/pages/schedule.tsx
+++ app/appointments/pages/schedule.tsx
@@ -1,13 +1,76 @@
 import { BlitzPage, useQuery } from "blitz"
 import React, { Suspense, useState } from "react"
+import Calendar from "react-calendar"
 import getAvailableSlots from "../queries/getAvailableSlots"
 
 const Scheduler = () => {
   const [availableSlots, { refetch }] = useQuery(getAvailableSlots, null)
   const [selected, setSelected] = useState()
 
+  const [selectedDay, setSelectedDay] = useState(new Date());
+
+  const onChange = (selectDay, event) => {
+    setSelectedDay(selectDay)
+    console.log("Test")
+    //todo show timeslots for day
+  }
+
   const onSubmit = (e: any) => {
     // Send selected to calendar owner
+  }
+  console.log("availableSlots: ", availableSlots);
+  const start = new Date("2020-11-25T11:00:00.000Z")
+  const end = new Date("2020-11-25T13:00:00.000Z")
+  const start1 = new Date("2020-11-25T13:00:00.000Z")
+  const end1 = new Date("2020-11-25T15:00:00.000Z")
+  const start2 = new Date("2020-11-25T15:00:00.000Z")
+  const end2 = new Date("2020-11-25T17:00:00.000Z")
+
+  const startn = new Date("2020-11-26T11:00:00.000Z")
+  const endn = new Date("2020-11-26T13:00:00.000Z")
+  const start1n = new Date("2020-11-26T13:00:00.000Z")
+  const end1n = new Date("2020-11-26T15:00:00.000Z")
+  const start2n = new Date("2020-11-26T15:00:00.000Z")
+  const end2n = new Date("2020-11-26T17:00:00.000Z")
+  const slots = [{"start": start, "end": end},{"start": start1, "end": end1},
+                {"start": start2, "end": end2},{"start": startn, "end": endn},
+                {"start": start1n, "end": end1n}, {"start": start2n, "end": end2n}]
+
+  const AvailableTimeSlotsSelection = () => {
+
+    function getTimeString(date){
+      return ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)
+    }
+
+    const timeSlotTiles = slots.map(slot => {
+      if (selectedDay.getDate() == slot.start.getDate() && selectedDay.getFullYear() == slot.start.getFullYear()) {
+        return (
+          <SingleTimeSlot
+            start={getTimeString(slot.start)}
+            end={getTimeString(slot.end)}
+          />
+        )
+      }
+    })
+
+    return(
+      <>
+      <p className="text-center col-span-full">
+        Please select a time slot.
+      </p>
+      <br/>
+      {timeSlotTiles}
+      </>
+    )
+  }
+
+  const SingleTimeSlot = ({start, end}) => {
+    return(
+      <div className="p-2 m-1 border border-gray-200 col-span-full">
+        <p>{start}-</p>
+        <p>{end}</p>
+      </div>
+    )
   }
 
   return (
@@ -20,16 +83,22 @@ const Scheduler = () => {
           </p>
         </div>
         <div className="border-t border-gray-200">
-          <div className="grid grid-cols-3 gap-6">
-            <div className="flex p-4 col-span-1  border-right border-r-2 border-gray-200">
+          <div className="grid grid-cols-full lg:grid-cols-3">
+            <div className="flex p-4 col-span-full lg:col-span-1 md:border-right md:border-r-2 md:border-gray-200">
               <div>Title, Description and other general stuff here</div>
             </div>
-            <div className="flex col-span-2">
-              <div>
-                calendar here with data with free slots highlighted. On click on date show available
-                slots
-              </div>
+            <div className="flex p-4 col-span-full lg:col-span-1">
+              <Calendar
+                onChange={onChange}
+                value={selectedDay}
+                maxDetail={"month"}
+                minDetail={"month"}
+                tileClassName={({ class1, date}) => date.getDay() === 3 ? true : false}
+              />
             </div>
+            <div className="flex p-4 col-span-full lg:col-span-1">
+              <AvailableTimeSlotsSelection/>
+              </div>
           </div>
         </div>
       </div>
