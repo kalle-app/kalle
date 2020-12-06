92ada87ec559a39fd77da101471573f1d888695d
Philip Weidenfeller <philip.weidenfeller@student.hpi.de>
2020-12-06 18:22:23 +0100

    remove conditional hook calls

--- app/appointments/pages/schedule/[uid]/[slug].tsx
+++ app/appointments/pages/schedule/[uid]/[slug].tsx
@@ -9,6 +9,7 @@ import { enUS } from "date-fns/locale"
 import getTimeSlots from "app/appointments/queries/getTimeSlots"
 import sendConfirmationMail from "app/components/createEmail/queries/sendConfirmationMail"
 import { PrimaryButton } from "app/components/buttons"
+import { Meeting } from "db"
 
 interface SchedulerProps {
   meetingSlug: string
@@ -18,20 +19,8 @@ interface SchedulerProps {
 const Scheduler = ({ meetingSlug, uid }: SchedulerProps) => {
   const [selectedTimeSlot, setSelectedTimeSlot] = useState({ start: null, end: null })
   const [selectedDay, setSelectedDay] = useState(new Date())
-
   const [meeting] = useQuery(getMeeting, meetingSlug)
-  if (!meeting) {
-    return <NoMeetingNotice text="This Meeting does not longer exist." />
-  }
 
-  const [connectedCalendars] = useQuery(getConnectedCalendars, meeting!.ownerId)
-
-  if (!(connectedCalendars && connectedCalendars[0])) {
-    return (
-      <NoMeetingNotice text="The person that invited you has removed his calender. The meeting can not longer be scheduled with this tool." />
-    )
-  }
-  // const slots = []
   var slots
   try {
     ;[slots] = useQuery(getTimeSlots, { meetingSlug: meetingSlug, calendarOwner: uid })
@@ -52,12 +41,12 @@ const Scheduler = ({ meetingSlug, uid }: SchedulerProps) => {
       return
     }
 
-    const hour = selectedTimeSlot.start.split(":")[0]
-    const minute = selectedTimeSlot.start.split(":")[1]
-    if (!hour || !minute) {
+    if (!selectedTimeSlot.start || !selectedTimeSlot.start) {
       alert("Invalid Time give")
       return
     }
+    const hour = selectedTimeSlot.start!.split(":")[0]
+    const minute = selectedTimeSlot.start!.split(":")[1]
 
     const appointment = {
       start: {
@@ -68,11 +57,11 @@ const Scheduler = ({ meetingSlug, uid }: SchedulerProps) => {
         minute: minute,
       },
       duration: {
-        hours: Math.floor(meeting.duration / 60),
-        minutes: meeting.duration % 60,
+        hours: Math.floor(meeting!.duration / 60),
+        minutes: meeting!.duration % 60,
       },
-      title: meeting.name,
-      description: meeting.description,
+      title: meeting!.name,
+      description: meeting!.description,
       method: "request",
       location: "Berlin",
       url: "www.kalle.app",
@@ -107,49 +96,68 @@ const Scheduler = ({ meetingSlug, uid }: SchedulerProps) => {
 
   return (
     <div className="container mx-auto p-4 mt-5">
-      <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-        <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">Schedule an Appointment</h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">
-            Select a timeslot of your preference
-          </p>
-        </div>
-        <div className="border-t border-gray-200">
-          <div className="grid grid-cols-full lg:grid-cols-3">
-            <div className="p-4 col-span-full lg:col-span-1 md:border-right md:border-r-2 md:border-gray-200">
-              <h1 className="text-xl font-medium">
-                {meeting.name.charAt(0).toUpperCase() + meeting.name.slice(1)}
-              </h1>
-              <p>{meeting.description}</p>
+      {!meeting && <NoMeetingNotice text="This Meeting does not longer exist." />}
+      {meeting && (
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">Schedule an Appointment</h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              Select a timeslot of your preference
+            </p>
+          </div>
+          <div className="border-t border-gray-200">
+            <div className="grid grid-cols-full lg:grid-cols-3">
+              <MeetingDetails meeting={meeting} />
+              <div className="p-4 col-span-full lg:col-span-1">
+                <DatePickerCalendar
+                  date={selectedDay}
+                  onDateChange={onChange}
+                  locale={enUS}
+                  modifiers={modifiers}
+                />
+              </div>
+              <div className="flex p-4 col-span-full lg:col-span-1">
+                <AvailableTimeSlotsSelection
+                  slots={slots}
+                  selectedDay={selectedDay}
+                  selectedTimeSlot={selectedTimeSlot}
+                  setSelectedTimeSlot={setSelectedTimeSlot}
+                />
+              </div>
             </div>
-            <div className="p-4 col-span-full lg:col-span-1">
-              <DatePickerCalendar
-                date={selectedDay}
-                onDateChange={onChange}
-                locale={enUS}
-                modifiers={modifiers}
-              />
-            </div>
-            <div className="flex p-4 col-span-full lg:col-span-1">
-              <AvailableTimeSlotsSelection
-                slots={slots}
-                selectedDay={selectedDay}
-                selectedTimeSlot={selectedTimeSlot}
-                setSelectedTimeSlot={setSelectedTimeSlot}
-              />
+            <div className="rounded-md shadow">
+              <a
+                href="#"
+                onClick={onSubmit}
+                className="w-1 m-4 flex float-right items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              >
+                Submit
+              </a>
             </div>
           </div>
-          <div className="rounded-md shadow">
-            <a
-              href="#"
-              onClick={onSubmit}
-              className="w-1 m-4 flex float-right items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-            >
-              Submit
-            </a>
-          </div>
         </div>
-      </div>
+      )}
+    </div>
+  )
+}
+
+interface MeetingDetailsProps {
+  meeting: Meeting
+}
+
+const MeetingDetails = ({ meeting }: MeetingDetailsProps) => {
+  const [connectedCalendars] = useQuery(getConnectedCalendars, meeting!.ownerId)
+  if (!(connectedCalendars && connectedCalendars[0])) {
+    return (
+      <NoMeetingNotice text="The person that invited you has removed his calender. The meeting can not longer be scheduled with this tool." />
+    )
+  }
+  return (
+    <div className="p-4 col-span-full lg:col-span-1 md:border-right md:border-r-2 md:border-gray-200">
+      <h1 className="text-xl font-medium">
+        {meeting.name.charAt(0).toUpperCase() + meeting.name.slice(1)}
+      </h1>
+      <p>{meeting.description}</p>
     </div>
   )
 }
