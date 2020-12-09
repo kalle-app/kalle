93356f7083166eede7129f9721b5c4d9cc4fbb8f
rohansaw <rohan.sawahn@student.hpi.de>
2020-12-09 18:01:27 +0100

    merged dev

--- app/users/queries/getConnectedCalendars.tsx
+++ app/users/queries/getConnectedCalendars.tsx
@@ -3,6 +3,7 @@ import { Ctx } from "blitz"
 
 export default async function getConnectedCalendars(_ = null, ctx: Ctx) {
   if (!ctx.session?.userId) return null
+  console.log(ctx.session.userId)
 
   const calendars = await db.connectedCalendar.findMany({
     where: { ownerId: ctx.session.userId },
