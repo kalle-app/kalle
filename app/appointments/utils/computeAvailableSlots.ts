539918deccd17a0e6263709f116fdd0edbb9377e
Simon Knott <info@simonknott.de>
2020-12-27 12:30:56 +0100

    Rewrite availability algorithm

--- app/appointments/utils/computeAvailableSlots.ts
+++ app/appointments/utils/computeAvailableSlots.ts
@@ -1,5 +1,4 @@
 import { ExternalEvent } from "app/caldav"
-import { areDatesOnSameDay } from "app/time-utils/comparison"
 import { TimeSlot } from "../types"
 
 function getTimePlusMinutes(date: Date, minutes: number) {
@@ -35,6 +34,14 @@ const dailySchedule = [
   { day: "friday", startTime: "9:00", endTime: "17:00", meetingId: 2 },
 ]
 
+function collides(a: TimeSlot, b: TimeSlot): boolean {
+  if (a.start >= b.start) {
+    return collides(b, a)
+  }
+
+  return a.end > b.start
+}
+
 interface ComputeAvailabilitySlotsArgs {
   between: TimeSlot
   durationInMilliseconds: number
@@ -46,60 +53,27 @@ export function computeAvailableSlots({
   durationInMilliseconds,
   takenSlots,
 }: ComputeAvailabilitySlotsArgs): TimeSlot[] {
-  const durationInMinutes = durationInMilliseconds / 1000 / 60
-  const duration = durationInMinutes
+  let cursor = between.start
 
-  takenSlots = takenSlots.sort((a, b) => a.start.getTime() - b.start.getTime())
+  const result: TimeSlot[] = []
 
-  // Build Array of DailySlots for every Day inbetween our dates
-  const slots: DailySlot[] = []
-  for (let dt = new Date(between.start); dt <= between.start; dt.setDate(dt.getDate() + 1)) {
-    slots.push({
-      date: new Date(dt),
-      free: [],
-      events: [],
-    })
+  const endOfSearch = new Date(+between.end - durationInMilliseconds)
+
+  while (cursor < endOfSearch) {
+    const potentialSlot: TimeSlot = {
+      start: cursor,
+      end: new Date(+cursor + durationInMilliseconds),
+    }
+
+    const collidingSlot = takenSlots.find((slot) => collides(slot, potentialSlot))
+
+    if (!collidingSlot) {
+      result.push(potentialSlot)
+      cursor = potentialSlot.end
+    } else {
+      cursor = collidingSlot.end
+    }
   }
 
-  // Map all externalEvents to the according day in slots
-  slots.forEach((day: DailySlot) => {
-    while (takenSlots.length !== 0 && areDatesOnSameDay(takenSlots[0].start, day.date)) {
-      day.events.push(takenSlots[0])
-      takenSlots.shift()
-    }
-  })
-
-  // Create free slots for every day
-  // For every day we start with the schedule startTime and then try to greedy find the next possible appointment
-  // As soon, as we cannot fit an appointment anymore there seems to be a blocking event, so we start to
-  // Greedy find the next fitting slot after that event
-  slots.forEach((day: DailySlot) => {
-    const weekdaySchedule = dailySchedule.find(
-      (schedule) => schedule.day === dayNames[day.date.getDay()]
-    )
-    if (weekdaySchedule) {
-      let time = makeDateTime(weekdaySchedule.startTime, day.date)
-      let endTime = makeDateTime(weekdaySchedule.endTime, day.date)
-      while (getTimePlusMinutes(time, duration) <= endTime) {
-        if (day.events.length === 0 || noCollision(time, day.events[0], duration)) {
-          day.free.push(time)
-          time = getTimePlusMinutes(time, duration)
-        } else {
-          // Currently default 0 minutes to next meeting, could be customized
-          time = getTimePlusMinutes(day.events[0].end, 0)
-          const eventToRemove = day.events[0]
-          day.events = day.events.filter((event) => event !== eventToRemove)
-        }
-      }
-    }
-  })
-
-  const freeSlots = slots.map((slot) =>
-    slot.free.map((item) => {
-      return { start: getTimePlusMinutes(item, 0), end: getTimePlusMinutes(item, duration) }
-    })
-  )
-  const freeSlotsFlat = freeSlots.flat()
-
-  return freeSlotsFlat
+  return result
 }
