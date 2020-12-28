e083054fbea2fffc384ebc6db5366dc5fbcece45
Simon Knott <info@simonknott.de>
2020-12-28 15:17:50 +0100

    Move johndoe over

--- cypress/user-data.ts
+++ cypress/user-data.ts
@@ -1,15 +1,3 @@
-export const johnDoe = {
-  email: "john.doe@kalle.app",
-  fullName: "John Doe",
-  password: "supersecurepassword",
+import { johnDoe as jd } from "../db/seeds"
 
-  calendars: {
-    baikal: {
-      name: "Baikal",
-      caldavAddress: "http://localhost:5232/dav.php/calendars/john.doe/test/",
-      username: "john.doe",
-      password: "root",
-      encryptedPassword: "ef87:y/u5eQgq3kbwuH8zeP/DVQ==:k3egrw==",
-    },
-  },
-}
+export const johnDoe = jd
--- db/seeds.ts
+++ db/seeds.ts
@@ -1,5 +1,20 @@
-import { johnDoe } from "cypress/user-data"
 import db from "db"
+
+export const johnDoe = {
+  email: "john.doe@kalle.app",
+  fullName: "John Doe",
+  password: "supersecurepassword",
+
+  calendars: {
+    baikal: {
+      name: "Baikal",
+      caldavAddress: "http://localhost:5232/dav.php/calendars/john.doe/test/",
+      username: "john.doe",
+      password: "root",
+      encryptedPassword: "ef87:y/u5eQgq3kbwuH8zeP/DVQ==:k3egrw==",
+    },
+  },
+}
 
 function mockHashPassword(pw: string) {
   if (pw === johnDoe.password) {
