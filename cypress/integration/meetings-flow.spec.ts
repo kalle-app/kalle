9a514a3325c1fbd9c4aa060f807c715126f0d4b7
Simon Knott <info@simonknott.de>
2021-01-13 17:33:34 +0100

    Increase waiting time for Messages

--- cypress/integration/meetings-flow.spec.ts
+++ cypress/integration/meetings-flow.spec.ts
@@ -50,7 +50,7 @@ it("Meetings Flow", () => {
 
   cy.contains("Submit!").click()
 
-  cy.wait(500)
+  cy.wait(1000)
 
   cy.request("http://localhost:8025/api/v2/messages").then((response) => {
     const {
