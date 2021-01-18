264611c3ae04b41a40ec5e2e2751e7a27d2eaeef
Philip Weidenfeller <philip.weidenfeller@student.hpi.de>
2021-01-18 19:43:25 +0100

    WIP social login cypress testing

--- cypress/integration/GoogleCalendarSpecs/addGoogleCalendar.spec.ts
+++ cypress/integration/GoogleCalendarSpecs/addGoogleCalendar.spec.ts
@@ -17,46 +17,46 @@ describe("A Google Calendar", () => {
   })
 })
 
-describe("Login", () => {
-  // WIP, see https://github.com/lirantal/cypress-social-logins/issues/78
-  it("Login through Google", () => {
-    const username = Cypress.env("googleSocialLoginUsername")
-    const password = Cypress.env("googleSocialLoginPassword")
-    const loginUrl = Cypress.env("loginUrl")
-    const cookieName = Cypress.env("cookieName")
-    const socialLoginOptions = {
-      username,
-      password,
-      loginUrl: Cypress.env("loginUrl"),
-      headless: false,
-      logs: true,
-      loginSelector: '[href="/auth/auth0/google-oauth2"]',
-      // loginSelector: 'a[href="https://www.googleapis.com/auth/calendar"]',
-      postLoginSelector: ".account-panel",
-    }
-    console.log("options: ", socialLoginOptions)
+// describe("Login", () => {
+//   // WIP, see https://github.com/lirantal/cypress-social-logins/issues/78
+//   it("Login through Google", () => {
+//     const username = Cypress.env("googleSocialLoginUsername")
+//     const password = Cypress.env("googleSocialLoginPassword")
+//     const loginUrl = Cypress.env("loginUrl")
+//     const cookieName = Cypress.env("cookieName")
+//     const socialLoginOptions = {
+//       username,
+//       password,
+//       loginUrl: Cypress.env("loginUrl"),
+//       headless: false,
+//       logs: true,
+//       loginSelector: '[href="/auth/auth0/google-oauth2"]',
+//       // loginSelector: 'a[href="https://www.googleapis.com/auth/calendar"]',
+//       postLoginSelector: ".account-panel",
+//     }
+//     console.log("options: ", socialLoginOptions)
 
-    return cy.task("GoogleSocialLogin", socialLoginOptions).then(({ cookies }) => {
-      console.log("clear cookies now :)")
+//     return cy.task("GoogleSocialLogin", socialLoginOptions).then(({ cookies }) => {
+//       console.log("clear cookies now :)")
 
-      cy.clearCookies()
-      console.log("return!")
+//       cy.clearCookies()
+//       console.log("return!")
 
-      const cookie = cookies.filter((cookie) => cookie.name === cookieName).pop()
-      if (cookie) {
-        console.log("cookie!")
+//       const cookie = cookies.filter((cookie) => cookie.name === cookieName).pop()
+//       if (cookie) {
+//         console.log("cookie!")
 
-        cy.setCookie(cookie.name, cookie.value, {
-          domain: cookie.domain,
-          expiry: cookie.expires,
-          httpOnly: cookie.httpOnly,
-          path: cookie.path,
-          secure: cookie.secure,
-        })
-        Cypress.Cookies.defaults({
-          preserve: cookieName,
-        })
-      }
-    })
-  })
-})
+//         cy.setCookie(cookie.name, cookie.value, {
+//           domain: cookie.domain,
+//           expiry: cookie.expires,
+//           httpOnly: cookie.httpOnly,
+//           path: cookie.path,
+//           secure: cookie.secure,
+//         })
+//         Cypress.Cookies.defaults({
+//           preserve: cookieName,
+//         })
+//       }
+//     })
+//   })
+// })
