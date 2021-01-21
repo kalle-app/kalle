dfb8c56622fbe26885b58c00143452c7c2c68be1
Philip Weidenfeller <philip.weidenfeller@student.hpi.de>
2021-01-21 17:38:31 +0100

    linting

--- app/meetings/pages/schedules.tsx
+++ app/meetings/pages/schedules.tsx
@@ -21,7 +21,11 @@ const MainContent = () => {
       <Card>
         <SectionHeader title="My Schedules" subtitle="View and Add new Schedule Presets" />
         <AllSchedules />
-        <SectionFooter id="add-schedule-footer" text="Add Schedule" action={() => showOverlay(true)} />
+        <SectionFooter
+          id="add-schedule-footer"
+          text="Add Schedule"
+          action={() => showOverlay(true)}
+        />
       </Card>
       <AddSchedule show={modalVisible} setVisibility={showOverlay} />
     </>
