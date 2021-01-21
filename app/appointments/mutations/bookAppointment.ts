4a752661a1550d38231b92d0b4c0848babae2fba
rohansaw <rohan.sawahn@student.hpi.de>
2021-01-21 16:57:19 +0100

    removed unused dependencies

--- app/appointments/mutations/bookAppointment.ts
+++ app/appointments/mutations/bookAppointment.ts
@@ -2,7 +2,7 @@ import { Ctx } from "blitz"
 import { Meeting } from "db"
 import createAppointmentEventMutation from "./createAppointmentEvent"
 import sendConfirmationMail from "./sendConfirmationMail"
-import { addMinutes, addSeconds, subMinutes } from "date-fns"
+import { subMinutes } from "date-fns"
 import reminderQueue from "../../api/queues/reminders"
 
 interface BookingInformation {
