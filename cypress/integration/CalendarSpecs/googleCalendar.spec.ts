502ffe5b79d7a7342dd8153378306393cb97014d
Philip Weidenfeller <philip.weidenfeller@student.hpi.de>
2021-01-27 23:20:34 +0100

    remove outcommented code in cypress

--- cypress/integration/CalendarSpecs/googleCalendar.spec.ts
+++ cypress/integration/CalendarSpecs/googleCalendar.spec.ts
@@ -1,7 +1,5 @@
-import { loginAs } from "../../login"
-import { url } from "../../support/url"
-import * as uuid from "uuid"
 import { johnDoe } from "../../../db/seed-data"
+import { loginAs } from "../../login"
 
 describe("A Google Calendar", () => {
   it("can be selected in settings", () => {
@@ -16,46 +14,4 @@ describe("A Google Calendar", () => {
   })
 })
 
-// TODO Manage Social Login, see https://github.com/lirantal/cypress-social-logins/issues/78
-// describe("Login", () => {
-//   it("Login through Google", () => {
-//     const username = Cypress.env("googleSocialLoginUsername")
-//     const password = Cypress.env("googleSocialLoginPassword")
-//     const loginUrl = Cypress.env("loginUrl")
-//     const cookieName = Cypress.env("cookieName")
-//     const socialLoginOptions = {
-//       username,
-//       password,
-//       loginUrl: Cypress.env("loginUrl"),
-//       headless: false,
-//       logs: true,
-//       loginSelector: '[href="/auth/auth0/google-oauth2"]',
-//       // loginSelector: 'a[href="https://www.googleapis.com/auth/calendar"]',
-//       postLoginSelector: ".account-panel",
-//     }
-//     console.log("options: ", socialLoginOptions)
-
-//     return cy.task("GoogleSocialLogin", socialLoginOptions).then(({ cookies }) => {
-//       console.log("clear cookies now :)")
-
-//       cy.clearCookies()
-//       console.log("return!")
-
-//       const cookie = cookies.filter((cookie) => cookie.name === cookieName).pop()
-//       if (cookie) {
-//         console.log("cookie!")
-
-//         cy.setCookie(cookie.name, cookie.value, {
-//           domain: cookie.domain,
-//           expiry: cookie.expires,
-//           httpOnly: cookie.httpOnly,
-//           path: cookie.path,
-//           secure: cookie.secure,
-//         })
-//         Cypress.Cookies.defaults({
-//           preserve: cookieName,
-//         })
-//       }
-//     })
-//   })
-// })
+// On the matter of testing the Google Social Login, see https://github.com/lirantal/cypress-social-logins/issues/78
