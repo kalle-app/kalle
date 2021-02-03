51e34f81ce5e9785ed668ee99aa38ce8eb46cf97
lukas.laskowski <lukas.laskowski@siemens.com>
2021-02-03 11:30:49 +0100

    added base_url env to make it usable for kalle.app domain

--- app/googlecalendar/helpers/GoogleClient.ts
+++ app/googlecalendar/helpers/GoogleClient.ts
@@ -2,9 +2,10 @@ import { google } from "googleapis"
 import { singleton } from "utils/singleton"
 
 export const getGoogleClient = singleton(() => {
+  const callbackString = process.env.BASE_URL + "/gcalOauth2Callback" // for demo reasons
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    "http://localhost:3000/gcalOauth2Callback"
+    callbackString
   )
 })
