cca20f669a285910c40cffa9e72f79fd61c0e82a
rohansaw <rohan.sawahn@student.hpi.de>
2021-02-07 14:04:09 +0100

    fixing cypress tests

--- cypress/integration/meetings-flow.spec.ts
+++ cypress/integration/meetings-flow.spec.ts
@@ -1,15 +1,16 @@
 import { loginAs } from "../login"
 import { url } from "../support/url"
 import * as uuid from "uuid"
-import { addDays, format } from "date-fns"
+import { addDays, format, isFriday, isSaturday } from "date-fns"
 
 import { johnDoe } from "../../db/seed-data"
 
 it("Meetings Flow", () => {
   const link = uuid.v4()
   const date = new Date()
+  const dateToSelect = isFriday(date) || isSaturday(date) ? addDays(date, 3) : addDays(date, 1)
   const dateStart = format(date, "dd.MM.y")
-  const dateEnd = format(addDays(date, 7), "dd.MM.y")
+  const dateEnd = format(addDays(date, 14), "dd.MM.y")
 
   loginAs(johnDoe)
 
@@ -45,7 +46,7 @@ it("Meetings Flow", () => {
 
   cy.visit(url(`/schedule/${johnDoe.username}/${link}`))
 
-  cy.contains(format(addDays(date, 1), "d")).click()
+  cy.contains(format(dateToSelect, "d")).click()
   cy.contains("10:30-10:45").click()
 
   cy.contains("Schedule!").click()
@@ -66,10 +67,7 @@ it("Meetings Flow", () => {
     expect(newestMail).to.exist
 
     expect(newestMail.Content.Headers.Subject[0]).to.equal(
-      `New appointment: My Test Meeting - 10:30, ${format(
-        addDays(date, 1),
-        "dd.MM.y"
-      )} mit john.doe`
+      `New appointment: My Test Meeting - 10:30, ${format(dateToSelect, "dd.MM.y")} mit john.doe`
     )
     expect(newestMail.Content.Headers.To[0]).to.equal("test-receiver@kalle.app")
   })
