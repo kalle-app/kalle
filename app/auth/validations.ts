5075df41585d0ba3b4a99f689d884b52d5563df0
Stefan Spangenberg <stefan.spangenberg@student.hpi.de>
2021-02-10 09:27:20 +0100

    Use variables for messages

--- app/auth/validations.ts
+++ app/auth/validations.ts
@@ -1,35 +1,43 @@
 import * as z from "zod"
 
+const messages = {
+  no_name: "Please enter a name",
+  short_username: "Username has to be at least 2 characters",
+  invalid_email: "Please enter a valid email address",
+  short_password: "Password must contain at least 10 characters",
+  long_password: "Password cannot contain more than 100 characters",
+}
+
 export const SignupInput = z.object({
-  name: z.string().min(1, { message: "Please enter a name" }),
-  username: z.string().min(3, { message: "Username has to be at least 2 characters" }),
-  email: z.string().email({ message: "Please enter valid email address" }),
+  name: z.string().min(1, { message: messages.no_name }),
+  username: z.string().min(3, { message: messages.short_username }),
+  email: z.string().email({ message: messages.invalid_email }),
   password: z
     .string()
-    .min(10, { message: "Password must contain at least 10 characters" })
-    .max(100, { message: "Password cannot contain more than 100 characters" }),
+    .min(10, { message: messages.short_password })
+    .max(100, { message: messages.long_password }),
 })
 export type SignupInputType = z.infer<typeof SignupInput>
 
 export const LoginInput = z.object({
-  email: z.string().email({ message: "Please enter valid email address" }),
+  email: z.string().email({ message: messages.invalid_email }),
   password: z
     .string()
-    .min(10, { message: "Password must contain at least 10 characters" })
-    .max(100, { message: "Password cannot contain more than 100 characters" }),
+    .min(10, { message: messages.short_password })
+    .max(100, { message: messages.long_password }),
 })
 export type LoginInputType = z.infer<typeof LoginInput>
 
 export const UpdateUserInput = z.object({
-  name: z.string().min(1, { message: "Please enter a name" }),
-  email: z.string().email({ message: "Please enter valid email address" }),
+  name: z.string().min(1, { message: messages.no_name }),
+  email: z.string().email({ message: messages.invalid_email }),
   password: z
     .string()
-    .min(10, { message: "Password must contain at least 10 characters" })
-    .max(100, { message: "Password cannot contain more than 100 characters" }),
+    .min(10, { message: messages.short_password })
+    .max(100, { message: messages.long_password }),
   repeatPassword: z
     .string()
-    .min(10, { message: "Password must contain at least 10 characters" })
-    .max(100, { message: "Password cannot contain more than 100 characters" }),
+    .min(10, { message: messages.short_password })
+    .max(100, { message: messages.long_password }),
 })
 export type UpdateUserInputType = z.infer<typeof UpdateUserInput>
