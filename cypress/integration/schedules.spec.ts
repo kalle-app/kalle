7dcf2c37de97d5d1cd59292107fb43a470a4b3b8
Philip Weidenfeller <philip.weidenfeller@student.hpi.de>
2021-02-23 09:37:26 +0100

    test custom schedule creation

--- cypress/integration/schedules.spec.ts
+++ cypress/integration/schedules.spec.ts
@@ -1,7 +1,6 @@
 import { loginAs } from "../login"
 import { url } from "../support/url"
 import * as uuid from "uuid"
-import { addDays, format, isFriday, isSaturday } from "date-fns"
 
 import { johnDoe } from "../../db/seed-data"
 
@@ -18,21 +17,35 @@ it("Schedules", () => {
   cy.visit(url("/schedules"))
   const name = uuid.v4()
   createDefaultSchedule(name)
-  cy.visit(url("/schedules"))
-    .contains(name)
+  cy.contains(name).parent().contains("Timezone").parent().parent().contains("Europe/Berlin")
+  cy.contains(name).parent().contains("Friday:").parent().parent().contains("9:00 - 17:00")
+  cy.contains(name).parent().contains("Saturday").should("not.exist")
+  //TODO test schedule delition, once implemented
+
+  const nameOfNotDefaultSchedule = uuid.v4()
+  cy.contains("Add Schedule").click()
+  cy.get("#name").type(nameOfNotDefaultSchedule)
+  cy.wait(2000)
+  cy.get("form").within(() => {
+    cy.get(".dropdown").get("a").click()
+    cy.get('input[placeholder="Type to filter..."]').type("seo")
+    cy.get('input[placeholder="Type to filter..."]').parent().contains("Madrid").should("not.exist")
+    cy.contains("Asia/Seoul").click()
+    cy.contains("Friday").parent().parent().find('input[value="09:00"]').type("{selectall}07:00")
+    cy.contains("Saturday").parent().parent().find('[type="checkbox"]').uncheck()
+  })
+  cy.contains("Save Schedule").click()
+  cy.contains(nameOfNotDefaultSchedule)
     .parent()
     .contains("Timezone")
     .parent()
     .parent()
-    .contains("Europe/Berlin")
-  cy.visit(url("/schedules"))
-    .contains(name)
+    .contains("Asia/Seoul")
+  cy.contains(nameOfNotDefaultSchedule)
     .parent()
     .contains("Friday:")
     .parent()
     .parent()
-    .contains("9:00 - 17:00")
-  cy.visit(url("/schedules")).contains(name).parent().contains("Saturday").should("not.exist")
-
-  //TODO test schedule delition, once implemented
+    .contains("7:00 - 17:00")
+  cy.contains(nameOfNotDefaultSchedule).parent().contains("Saturday")
 })
