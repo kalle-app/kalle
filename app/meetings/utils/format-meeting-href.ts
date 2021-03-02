711253a8b6a5aacdcb88ff0a09a3479d040766ae
Simon Knott <info@simonknott.de>
2021-03-02 20:18:59 +0100

    Rename "generalUtils"

--- app/meetings/utils/format-meeting-href.ts
+++ app/meetings/utils/format-meeting-href.ts
@@ -1,5 +1,5 @@
 import type { Meeting } from "db"
-import { getOrigin } from "utils/generalUtils"
+import { getOrigin } from "utils/origin"
 
 export function formatMeetingHref(meeting: Pick<Meeting, "ownerName" | "link">) {
   return `${getOrigin()}/schedule/${meeting.ownerName}/${meeting.link}`
