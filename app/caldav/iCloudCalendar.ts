be24416ffb5fc2f05815b951c2aff5f98b95e0d9
lukas.laskowski <lukas.laskowski@siemens.com>
2021-03-03 20:15:52 +0100

    gets correct url from icloud

--- app/caldav/iCloudCalendar.ts
+++ app/caldav/iCloudCalendar.ts
@@ -1,20 +1,53 @@
 import { CalendarConnectionDetails, makeRequestTo } from "./index"
-
+import * as jsdom from "jsdom"
+const {JSDOM} = jsdom
 export async function getConnectionString(calendar: CalendarConnectionDetails) {
   calendar.url = "https://caldav.icloud.com"
-  console.log("making the request")
-  console.log(calendar)
   const z = await makeRequestTo(calendar, {
     headers: { DEPTH: 0 },
     method: "PROPFIND",
     data: `
-                <propfind xmlns='DAV:'>
-                    <prop>
-                        <current-user-principal/>
-                    </prop>
-                </propfind>
+            <propfind xmlns='DAV:'>
+                <prop>
+                    <current-user-principal/>
+                </prop>
+            </propfind>
             `,
     }
   )
-  console.log("response", z.data.toString())
+  
+  var xmlDoc = new JSDOM(z.data.toString(), {contentType: 'text/xml'})
+  const principal = xmlDoc.window.document.querySelector("multistatus")
+                        .querySelector('propstat')
+                        .querySelector('prop')
+                        .querySelector('current-user-principal')
+                        .textContent
+
+  calendar.url = calendar.url + principal.slice(0, -1);
+  //calendar.url = calendar.url.slice(0, -1);
+  const a = await makeRequestTo(calendar, {
+    headers: { DEPTH: 0 },
+    method: "PROPFIND",
+    data: `
+            <propfind xmlns='DAV:' xmlns:cd='urn:ietf:params:xml:ns:caldav'>
+                <prop>
+                    <cd:calendar-home-set/>
+                </prop>
+            </propfind>
+            `,
+    }
+  )
+  
+  var xmlDoc = new JSDOM(a.data.toString(), {contentType: 'text/xml'})
+  return xmlDoc.window.document
+            .querySelector("multistatus")
+            .querySelector('response')
+            .querySelector('propstat')
+            .querySelector('prop')
+            .querySelector('calendar-home-set')
+            .querySelector('href')
+            .textContent
+  //console.log(a.data.toString())
+  
+  //.console.log(xmlDoc.getElementsByTagName('multistatus')[0].children[0].textContent)
 }
