f569253ea31529a637d277990e7180c351d4b231
lukas.laskowski <lukas.laskowski@siemens.com>
2021-03-04 23:00:05 +0100

    added getter for refresh token successfully

---
+++ app/outlookcalendar/helper/authHelpers.ts
@@ -0,0 +1,33 @@
+import * as request from "request"
+
+import { checkEnvVariable } from "utils/checkEnvVariables";
+import { baseURL, client_id, client_secret, grant_type, redirect_uri, scope } from "../constants";
+
+export const requestToken = async (code: string): Promise<{access_token: string, refresh_token: string}> => { //TODO remove any
+    const url = new URL(baseURL + 'token');
+
+    var options = {
+        'method': 'POST',
+        'url': url.href,
+        'formData': buildBody(code)
+    }
+
+    return new Promise((resolve, reject) => {request(options, function (error, response) {
+        if (error) reject(error);
+        const res = JSON.parse(response.body)
+        resolve({refresh_token: res.refresh_token, access_token: res.access_token})
+    })})
+}
+
+const buildBody = (code: string): object => {
+    checkEnvVariable('MICROSOFTCLIENTSECRET')
+
+    return {
+        'client_id': client_id!,
+        'scope': scope,
+        'code': code,
+        'redirect_uri': redirect_uri,
+        'grant_type': grant_type,
+        'client_secret': client_secret!
+    }
+}
--- app/outlookcalendar/queries/handleOAuthCode.ts
+++ app/outlookcalendar/queries/handleOAuthCode.ts
@@ -1,5 +1,11 @@
+import {requestToken} from "../helper/authHelpers"
+
 export default async function handleOAuthCode(code: string){
+    const token = await requestToken(code);
+    
+    console.log(token)
     // build new request to get token
     // make request
     // store values in db
 }
+
