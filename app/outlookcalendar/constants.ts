62d44b78335d015162022ffb5907b2690833e5b5
lukas.laskowski <lukas.laskowski@siemens.com>
2021-03-04 21:41:44 +0100

    adds baseURL to constants

--- app/outlookcalendar/constants.ts
+++ app/outlookcalendar/constants.ts
@@ -1,6 +1,7 @@
-export const tenant = "common"
+const tenant = "common"
 export const client_id = process.env.MICROSOFTCLIENTID
 export const redirect_uri = "http://localhost:3000/outlookRedirect"
 export const response_mode = "query"
 export const scope =  "offline_access user.read mail.read"
 export const response_type = "code"
+export const baseURL = "https://login.microsoftonline.com/" + tenant + "/oauth2/v2.0/"
