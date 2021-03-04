ff699c58d3fbd59f22cf89289a2f1b4ec1926cb5
lukas.laskowski <lukas.laskowski@siemens.com>
2021-03-04 21:37:21 +0100

    reformat code

--- app/outlookcalendar/pages/outlookRedirect.tsx
+++ app/outlookcalendar/pages/outlookRedirect.tsx
@@ -1,8 +1,8 @@
 import Layout from "app/layouts/Layout"
-import { BlitzPage, Link, useMutation, useRouterQuery } from "blitz"
+import { BlitzPage, Link, useRouterQuery } from "blitz"
 import { Suspense, useState } from "react"
 import { Button, Form } from "react-bootstrap"
-import {handleOAuthCode} from "../queries/handleOAuthCode"
+import handleOAuthCode from "../queries/handleOAuthCode"
 interface credentials {
   account: object
   scopes: string[]
@@ -11,79 +11,79 @@ interface credentials {
  * This gets a code as a query parameter. This code needs to be sent to googleapi which returns a refresh_token. The refresh_tokem is used to generate a session_access_token.
  */
 function OAuthCallbackPage() {
-  const [isError, setIsError] = useState(false)
-  const [isCalenderAdded, setIsCalenderAdded] = useState(false)
-  const [calendarName, setCalendarName] = useState("Your Google Calendar")
-  const [handleAUth]
-  let { code } = useRouterQuery()
+    const [isError, setIsError] = useState(false)
+    const [isCalenderAdded, setIsCalenderAdded] = useState(false)
+    const [calendarName, setCalendarName] = useState("Your Outlook Calendar")
 
-  const SettingsLink = () => {
-    const hanldeCode = async () => {
+    let { code } = useRouterQuery()
+    const handleCode = async () => {
         setIsError(false)
         try {
           if (!code || Array.isArray(code))
-            return <p>Google Authentication failed with Code {code}. Please try again.</p>
+            return <p>Microsoft Authentication failed with Code {code}. Please try again.</p>
 
           return await handleOAuthCode(code)
         } catch (error) {
           setIsError(true)
         }
       }
-    return (
-      <Link href={"/settings"}>
-        <Button variant="secondary" href={"/settings"}>
-          Go to Calendar Settings
-        </Button>
-      </Link>
-    )
-  }
+    const SettingsLink = () => {
+        return (
+        <Link href={"/settings"}>
+            <Button variant="secondary" href={"/settings"}>
+            Go to Calendar Settings
+            </Button>
+        </Link>
+        )
+    }
 
-  return (
-    <>
-      {isError ? (
+    return (
         <>
-          <h1>An error has occurred</h1>
-          <p>
-            Please try again. Notice that this Google Calendar might already be connected to Kalle.
-            If so please edit or delete it in the calendar settings.
-          </p>
-          <SettingsLink />
+        {isError ? (
+            <>
+            <h1>An error has occurred</h1>
+            <p>
+                Please try again. Notice that this Microsoft Calendar might already be connected to Kalle.
+                If so please edit or delete it in the calendar settings.
+            </p>
+            <SettingsLink />
+            </>
+        ) : isCalenderAdded ? (
+            <>
+            <h3>Great! {calendarName} has been added.</h3>
+            <SettingsLink />
+            </>
+        ) : (
+            <>
+            <h1>Your Authentication was succesful.</h1>
+            <h3>Last step. Please choose a name for your new Calendar.</h3>
+            <Form>
+                <Form.Group controlId="formGoogleCalendarName">
+                <Form.Label>Calendar Name</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Enter a name you'd like for your calendar"
+                    onChange={(event) => setCalendarName(event.target.value)}
+                />
+                <Form.Text className="text-muted">
+                    This name helps you to recognize the calendar. For example "Private" or "My project
+                    with Alice".
+                </Form.Text>
+                </Form.Group>
+            </Form>
+            <Button
+                variant="primary"
+                onClick={async () => {
+                    await handleCode()
+                if (!isError) setIsCalenderAdded(true)
+                }}
+            >
+                Add Calendar
+            </Button>
+            </>
+        )}
         </>
-      ) : isCalenderAdded ? (
-        <>
-          <h3>Great! {calendarName} has been added.</h3>
-          <SettingsLink />
-        </>
-      ) : (
-        <>
-          <h1>Your Authentication was succesful.</h1>
-          <h3>Last step. Please choose a name for your new Calendar.</h3>
-          <Form>
-            <Form.Group controlId="formGoogleCalendarName">
-              <Form.Label>Calendar Name</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter a name you'd like for your calendar"
-                onChange={(event) => setCalendarName(event.target.value)}
-              />
-              <Form.Text className="text-muted">
-                This name helps you to recognize the calendar. For example "Private" or "My project
-                with Alice".
-              </Form.Text>
-            </Form.Group>
-          </Form>
-          <Button
-            variant="primary"
-            onClick={async () => {
-              if (!isError) setIsCalenderAdded(true)
-            }}
-          >
-            Add Calendar
-          </Button>
-        </>
-      )}
-    </>
-  )
+    )
 }
 
 const oAuth2Callback: BlitzPage = () => {
