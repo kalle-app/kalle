715724f82a96056b04118d30084bf721d05eb388
sspangenberg <49531479+sspangenberg@users.noreply.github.com>
2021-03-06 09:43:40 +0100

    Adjust default times

--- app/meetings/components/schedules/AddScheduleModal.tsx
+++ app/meetings/components/schedules/AddScheduleModal.tsx
@@ -64,8 +64,8 @@ const initialSchedule = {
   wednesday: { blocked: false, start: "09:00", end: "17:00" },
   thursday: { blocked: false, start: "09:00", end: "17:00" },
   friday: { blocked: false, start: "09:00", end: "17:00" },
-  saturday: { blocked: true, start: "00:00", end: "00:00" },
-  sunday: { blocked: true, start: "00:00", end: "00:00" },
+  saturday: { blocked: true, start: "09:00", end: "17:00" },
+  sunday: { blocked: true, start: "09:00", end: "17:00" },
 }
 
 const AddSchedule = (props: AddScheduleProps) => {
