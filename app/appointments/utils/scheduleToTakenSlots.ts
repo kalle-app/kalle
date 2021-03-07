616bc3c57f24046aba151ae280dfbae01bb8e098
Simon Knott <info@simonknott.de>
2021-03-07 14:33:08 +0100

    Revert "include last slot for last day of meeting"
    This reverts commit 188bd09a6c89f9cd82d6fbc3cdb962bca5b7be6c.

--- app/appointments/utils/scheduleToTakenSlots.ts
+++ app/appointments/utils/scheduleToTakenSlots.ts
@@ -46,16 +46,18 @@ export function scheduleToTakenSlots(
   }
 
   const result: TimeSlot[] = []
-  let cursor = between.start
 
-  //we need to include the blocked slot between the end of the last day (between.end) and beginning of the next day (addDays(between.end, 1))
-  while (cursor <= addDays(between.end, 1)) {
+  let cursor = between.start
+  while (cursor <= between.end) {
     const slot: TimeSlot = {
       start: endOfLastWorkDayBefore(cursor, schedule, timezone),
       end: startOfFirstWorkDayOnOrAfter(cursor, schedule, timezone),
     }
+
     result.push(slot)
+
     cursor = addDays(slot.end, 1)
   }
+
   return result
 }
